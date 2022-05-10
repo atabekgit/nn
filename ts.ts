0183cec4b229af80c15aa816a87329a8e90a172a
atabe <atabekgithub23>
2022-05-10 12:14:43 +0600

    test

---
+++ ts.ts
@@ -0,0 +1,15 @@
+function addNumbers(a: number, b: number) {
+    return a + b;
+}
+
+var sum: number = addNumbers(10, 15)
+
+let employee:{
+    id:number;
+    name:string;
+}
+employee = {
+    id:100,
+    name:"John"
+}
+
